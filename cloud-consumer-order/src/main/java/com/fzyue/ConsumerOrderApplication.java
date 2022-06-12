69af0195aa70b64afbc5b040b4af6c600a957980
xiaoyu <xiaoyu@mjoys.com>
2022-06-13 01:05:05 +0800

    init

---
+++ cloud-consumer-order/src/main/java/com/fzyue/ConsumerOrderApplication.java
@@ -0,0 +1,14 @@
+package com.fzyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ConsumerOrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerOrderApplication.class, args);
+    }
+}
